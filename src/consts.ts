53201455d3a0169142bb113269c2912963ba6160
uvacoder <uva22940coder@gmail.com>
2023-08-16 04:19:02 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -9,27 +9,27 @@ export const ROUTES = [
 export const SOCIAL = [
   {
     label: "Website",
-    href: "https://ricardochu.com",
+    href: "https://art4helen.vercel.app",
     icon: "mdi:web",
   },
   // {
   //   label: "GitHub",
-  //   href: "https://github.com/kypexfly",
+  //   href: "https://github.com/uvacoder",
   //   icon: "mdi:github",
   // },
   // {
   //   label: "LinkedIn",
-  //   href: "https://www.linkedin.com/in/ricardochuzheng/",
+  //   href: "https://www.linkedin.com/in/alecbcampbell/",
   //   icon: "mdi:linkedin",
   // },
 ] as const;
 
 export const PERSONAL_INFO = {
-  name: "Ricardo Chu",
-  title: "Kypexfly's Art",
-  subtitle: "A collection of my best work in one place",
-  role: "FrontEnd Developer - Digital Artist",
-  contact: "mailto:rchuzheng@gmail.com",
+  name: "Helen Ward",
+  title: "Helen's Art",
+  subtitle: "A collection of my best work.",
+  role: "  "Television Show Artist - Digital Artist",
+  contact: "mailto:hward136@gmail.com",
   avatar: "https://avatars.githubusercontent.com/u/92066360?v=4",
   about: "",
 } as const;
