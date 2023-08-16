def4f9142913ba1a29f79da54da5c091c205f120
uvacoder <uva22940coder@gmail.com>
2023-08-16 05:51:57 -0400

    Update gallery.ts

--- src/gallery.ts
+++ src/gallery.ts
@@ -1,35 +1,35 @@
 export const GALLERY = [
   {
-    title: "Glass",
-    src: "https://i.imgur.com/uNqMbaF.png",
+    title: "Eye on You",
+    src: "https://res.cloudinary.com/freeunioncoder/image/upload/v1677266172/FavDribbble/F677E535-BD90-44D2-856E-A4B2BBAA45B3.png",
   },
   {
-    title: "Moon",
-    src: "https://i.imgur.com/JQC2Cr6.png",
+    title: "Bad Pony",
+    src: "https://res.cloudinary.com/freeunioncoder/image/upload/v1675140158/FavDribbble/8CA08123-B117-495F-9F02-50EAD5A8B019.png",
   },
   {
-    title: "Isometric City",
-    src: "https://i.imgur.com/Nrtn6yD.png",
+    title: "Cow Ow",
+    src: "https://res.cloudinary.com/freeunioncoder/image/upload/v1675140156/FavDribbble/7431FEE0-3A3D-4140-8389-016E5F9655D8.png",
   },
   {
-    title: "Abstract Shapes",
-    src: "https://i.imgur.com/UwvOLLA.png",
+    title: "Player Player",
+    src: "https://res.cloudinary.com/freeunioncoder/image/upload/v1675140162/FavDribbble/F3CC711A-1B74-437D-B8B8-A4BC74913492.png",
   },
   {
-    title: "Isometric Cubes",
-    src: "https://i.imgur.com/zaiBzN4.png",
+    title: "Rap Master",
+    src: "https://res.cloudinary.com/freeunioncoder/image/upload/v1675140152/FavDribbble/B3254DDB-532C-492C-BDF4-399696D38C62.png",
   },
   {
-    title: "Astronaut",
-    src: "https://i.imgur.com/AOkTqSJ.png",
+    title: "Personal Office",
+    src: "https://res.cloudinary.com/freeunioncoder/image/upload/v1677266150/FavDribbble/6F6F6994-92BE-4F54-A838-89F04738D47F.png",
   },
   {
-    title: "Abstract Fx",
-    src: "https://i.imgur.com/UgGlUKe.png",
+    title: "WoodWorks",
+    src: "https://res.cloudinary.com/freeunioncoder/image/upload/v1675140629/logos-nextjs/13F0EF1A-297A-4B90-9626-7201230A83B4.jpg",
   },
   {
-    title: "Abstract  Storm",
-    src: "https://i.imgur.com/JLRFv3y.png",
+    title: "Got Wood",
+    src: "https://res.cloudinary.com/freeunioncoder/image/upload/v1675140603/logos-nextjs/5D3F99FB-952C-40DC-86E1-BA8393F7A16F.jpg",
   },
   {
     title: "Neon City",
@@ -106,41 +106,5 @@ export const GALLERY = [
   {
     title: "Psycho-Pass",
     src: "https://i.imgur.com/Do2sELz.png",
-  },
-  {
-    title: "Painting",
-    src: "https://i.imgur.com/BwrLjyQ.png",
-  },
-  {
-    title: "Digital Painting 1",
-    src: "https://i.imgur.com/hIFF37M.png",
-  },
-  {
-    title: "Collage",
-    src: "https://i.imgur.com/A76n4Fz.png",
-  },
-  {
-    title: "Big Disaster",
-    src: "https://i.imgur.com/O2HWiNf.png",
-  },
-  {
-    title: "Landscape",
-    src: "https://i.imgur.com/lxJsk2H.png",
-  },
-  {
-    title: "Abstract Smudge",
-    src: "https://i.imgur.com/Dc2G9Ks.png",
-  },
-  {
-    title: "Warrior",
-    src: "https://i.imgur.com/FRG2WUx.png",
-  },
-  {
-    title: "Abstract Smudge 2",
-    src: "https://i.imgur.com/WweSwgz.png",
-  },
-  {
-    title: "Infinity",
-    src: "https://i.imgur.com/Mac1ZNT.png",
   },
 ];
